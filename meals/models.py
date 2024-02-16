1db687c9cb4cbb7b2a94f9f03a0704d4b11dd73b
Omarmoatz <omar.raouf.002@gmail.com>
2024-02-16 16:01:05 +0200

    add requerments.txt file

--- meals/models.py
+++ meals/models.py
@@ -21,7 +21,7 @@ class Category(models.Model):
     
     class Meta:
         verbose_name = "Category"
-        verbose_name_plural = "Categorys"
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name

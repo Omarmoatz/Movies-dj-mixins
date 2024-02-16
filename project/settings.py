f0a2a1df0c04e5db12e80ad1d61420d580d3773a
Omarmoatz <omar.raouf.002@gmail.com>
2024-02-16 14:52:58 +0200

    start new app meal

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'meal',
+
+    # 3rd part apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [

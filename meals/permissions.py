7961260c1f5b2a32640738976cffb63d697472b4
Omarmoatz <omar.raouf.002@gmail.com>
2024-02-19 17:44:17 +0200

    edit custom Permissions

--- meals/permissions.py
+++ meals/permissions.py
@@ -1,17 +1,34 @@
 from rest_framework import permissions
 
 class IsStaffEditorPermission(permissions.DjangoModelPermissions):
+
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
     def has_permission(self, request, view):
-        user = request.user
-        print(user.get_all_permissions())
-        if user.is_staff:
-            if user.has_perm("meals.view_meal"):
-                return True
-            if user.has_perm("meals.add_meal"):
-                return True
-            if user.has_perm("meals.change_meal"):
-                return True
-            if user.has_perm("meals.delete_meal"):
-                return True
+        if not request.user.is_staff:
             return False
         return super().has_permission(request, view)
+
+
+    # def has_permission(self, request, view):
+    #     user = request.user
+    #     print(user.get_all_permissions())
+    #     if user.is_staff:
+    #         if user.has_perm("meals.view_meal"):   # 'appname.verb_model'
+    #             return True
+    #         if user.has_perm("meals.add_meal"):
+    #             return True
+    #         if user.has_perm("meals.change_meal"):
+    #             return True
+    #         if user.has_perm("meals.delete_meal"):
+    #             return True
+    #         return False
+    #     return super().has_permission(request, view)
